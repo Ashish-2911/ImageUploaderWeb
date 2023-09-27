90f659efd39fc406f7e08a56367475e00f004bb8
Ashish001 <ashishchouhan57488@gmail.com>
2023-09-27 15:35:13 +0530

    initial commit

---
+++ imageuploader/myapp/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from .forms import ImageForm
+from .models import Image
+
+# Create your views here.
+
+def home(request):
+    if request.method == "POST":
+        form = ImageForm(request.POST , request.FILES)
+        if form.is_valid():
+            form.save()
+    form = ImageForm()
+    img = Image.objects.all()
+    return render(request,'myapp/home.html',{'form':form,'img':img })
+
+
+def photoShow(request,id):
+    # if request.method == 'POST':
+    images = Image.objects.get(pk=id)
+    return render(request,'myapp/photoshow.html',{'img':images})
+
+#website to be continue......
+#xyz
