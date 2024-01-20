0551ac55ed6d1ef7a3d1da1ad16596c69b68636f
Ashish001 <ashishchouhan57488@gmail.com>
2024-01-20 17:34:36 +0530

    update

--- myapp/views.py
+++ myapp/views.py
@@ -8,12 +8,10 @@ def home(request):
     if request.method == "POST":
         form = ImageForm(request.POST , request.FILES)
         if form.is_valid():
-            form.save()
-        img = Image.objects.all()
-        return render(request,'myapp/home.html',{'img':img })
-    else:
-        form = ImageForm()           
-        return render(request,'myapp/home.html',{'form':form})
+            form.save()       
+    form = ImageForm() 
+    img = Image.objects.all()          
+    return render(request,'myapp/home.html',{'form':form,'img':img})
 
 
 def photoShow(request,id):
